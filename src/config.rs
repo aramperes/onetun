96e18edd190bcbd8072536d2e71fadab850a70e2
Jackson Coxson <jkcoxson@gmail.com>
2022-06-23 23:10:11 -0600

    Invert logic for IP version mismatch

--- src/config.rs
+++ src/config.rs
@@ -238,8 +238,8 @@ impl Config {
         let endpoint_bind_addr = if let Some(addr) = matches.value_of("endpoint-bind-addr") {
             let addr = parse_addr(Some(addr)).with_context(|| "Invalid host address")?;
             // Make sure the host address and endpoint address are the same IP version
-            if addr.ip().is_ipv6() && endpoint_addr.ip().is_ipv6()
-                || (addr.ip().is_ipv4() && endpoint_addr.ip().is_ipv4())
+            if addr.ip().is_ipv6() != endpoint_addr.ip().is_ipv6()
+                || (addr.ip().is_ipv4() != endpoint_addr.ip().is_ipv4())
             {
                 return Err(anyhow::anyhow!(
                     "Host address and endpoint address must be the same IP version"
